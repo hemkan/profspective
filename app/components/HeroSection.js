329d0aa9b05169008cb2be95b8491fc93041f1b8
hemkan <hemkano1403@gmail.com>
2024-08-24 19:35:47 -0400

    add welcome animation

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -1,128 +1,133 @@
 "use client";
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
-import Image from "next/image";
+import { Box, Button, Typography, Container } from "@mui/material";
 import Link from "next/link";
+import Lottie from "lottie-react";
+import WelcomeAnimation from "../../public/WelcomeAnimation.json";
 
 export default function HeroSection() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: {
-          xs: "column", // Stack elements vertically on small screens
-          sm: "row",    // Arrange elements horizontally on medium and larger screens
-        },
-        alignItems: "center",
-        justifyContent: "space-evenly",
-        padding: "40px 20px",
-        backgroundColor: "#13131E",
-        height: "80vh",
-        textAlign: {
-          xs: "center", // Center text on small screens
-          sm: "left",   // Align text to the left on larger screens
-        },
-      }}
-    >
+    <Container maxWidth="lg">
       <Box
         sx={{
-          flex: 1,
-          maxWidth: "600px",
+          display: "flex",
+          flexDirection: {
+            xs: "column", // Stack elements vertically on small screens
+            sm: "row", // Arrange elements horizontally on medium and larger screens
+          },
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          padding: "40px 20px",
+          backgroundColor: "#13131E",
+          height: "80vh",
+          textAlign: {
+            xs: "center", // Center text on small screens
+            sm: "left", // Align text to the left on larger screens
+          },
         }}
       >
-        <Typography
-          variant="h1"
-          sx={{
-            fontSize: {
-              xs: "28px", // Font size for small screens
-              sm: "40px", // Font size for larger screens
-            },
-            fontWeight: "bold",
-            marginBottom: "16px",
-            color: "#FFFFFF",
-            fontFamily: "Righteous",
-          }}
-        >
-          Find Your Ideal Professor
-        </Typography>
-        <Typography
-          variant="h6"
-          sx={{
-            fontSize: {
-              xs: "16px", // Font size for small screens
-              sm: "18px", // Font size for larger screens
-              color: "#FFFFFF",
-            },
-            marginBottom: "32px",
-          }}
-        >
-          Discover the professors who match your learning style and academic goals, with ProfSpective’s personalized recommendations and student insights.
-        </Typography>
         <Box
           sx={{
-            display: "flex",
-            flexDirection: {
-              xs: "column", // Stack buttons vertically on small screens
-              sm: "row",    // Arrange buttons horizontally on larger screens
-            },
-            gap: "16px",
+            flex: 1,
+            maxWidth: "600px",
           }}
         >
-        <Link  href="/">
-          <Button
-            variant="contained"
+          <Typography
+            variant="h1"
             sx={{
-            fontFamily: "Righteous",
               fontSize: {
-                xs: "14px",
-                sm: "16px",
-                color: "#E0DFFE",
+                xs: "28px", // Font size for small screens
+                sm: "40px", // Font size for larger screens
               },
-              backgroundColor: "#6E6ADE",
-              borderRadius: "20px",
-              padding: "12px 24px",
-            }}
-          >
-            Get Started
-          </Button>
-          </Link>
-          <Link  href="/">
-          <Button
-            variant="outlined"
-            color="primary"
-            sx={{
+              fontWeight: "bold",
+              marginBottom: "16px",
+              color: "#FFFFFF",
               fontFamily: "Righteous",
-              fontSize: {
-                xs: "14px",
-                sm: "16px",
-                color: "#6E6ADE",
-              },
-              borderColor: "#6E6ADE",
-              borderRadius: "20px",
-              padding: "12px 24px",
             }}
           >
-            How It Works
-          </Button>
-          </Link>
+            Find Your Ideal Professor
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{
+              fontSize: {
+                xs: "16px", // Font size for small screens
+                sm: "18px", // Font size for larger screens
+                color: "#FFFFFF",
+              },
+              marginBottom: "32px",
+            }}
+          >
+            Discover the professors who match your learning style and academic
+            goals, with ProfSpective’s personalized recommendations and student
+            insights.
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: {
+                xs: "column", // Stack buttons vertically on small screens
+                sm: "row", // Arrange buttons horizontally on larger screens
+              },
+              gap: "16px",
+            }}
+          >
+            <Link href="/">
+              <Button
+                variant="contained"
+                sx={{
+                  fontFamily: "Righteous",
+                  fontSize: {
+                    xs: "14px",
+                    sm: "16px",
+                    color: "#E0DFFE",
+                  },
+                  backgroundColor: "#6E6ADE",
+                  borderRadius: "20px",
+                  padding: "12px 24px",
+                }}
+              >
+                Get Started
+              </Button>
+            </Link>
+            <Link href="/">
+              <Button
+                variant="outlined"
+                color="primary"
+                sx={{
+                  fontFamily: "Righteous",
+                  fontSize: {
+                    xs: "14px",
+                    sm: "16px",
+                    color: "#6E6ADE",
+                  },
+                  borderColor: "#6E6ADE",
+                  borderRadius: "20px",
+                  padding: "12px 24px",
+                }}
+              >
+                How It Works
+              </Button>
+            </Link>
+          </Box>
         </Box>
-      </Box>
-      <Box
+        <Box
           sx={{
             position: "relative",
-            width: "420px",
-            height: "420px", // Set a fixed height for the image container
-            maxWidth: "420px", // Set a max width for the image container
+            width: "500px",
+            height: "500px", // Set a fixed height for the image container
+            maxWidth: "500px", // Set a max width for the image container
           }}
         >
-          <Image
-            src="/ProfspectiveHero.png" // Update this path to your image file
-            alt="ProfSpective"
-            layout="fill" // Makes the image fill its container
-            objectFit="cover" // Ensures the image covers the container without distortion
-            style={{ borderRadius: "8px" }} // Optional: Add rounded corners
-          />
+          <div style={{ transform: "scale(1.4)", transformOrigin: "center" }}>
+            <Lottie
+              animationData={WelcomeAnimation}
+              loop={true}
+              alt="ProfSpective"
+            />
+          </div>
         </Box>
-    </Box>
+      </Box>
+    </Container>
   );
 }
