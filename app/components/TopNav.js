eeed397d74a7606b82c2be27ddb4d7e83611a5f2
GauravKano <gaurav.kanoujia2006@gmail.com>
2024-08-23 23:23:02 -0400

    Fix Error

--- app/components/TopNav.js
+++ app/components/TopNav.js
@@ -1,46 +1,61 @@
 "use client";
 import { Typography, Toolbar, Button, AppBar, Box } from "@mui/material";
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function TopNav() {
   return (
     <AppBar position="static">
       <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Typography variant="h6" sx={{ color: colors.text }}>
-          Swift-Cards
+        <Typography variant="h6">
+          <Link
+            href="/"
+            style={{
+              textDecoration: "none",
+              fontWeight: "bold",
+              color: "black",
+            }}
+          >
+            Swift-Cards
+          </Link>
         </Typography>
         <Box sx={{ display: "flex", gap: 2 }}>
           <SignedOut>
-            <Button
-              color="inherit"
-              sx={{
-                color: colors.text,
-                textTransform: "none",
-                fontSize: "12px",
-              }}
-              href="/sign-in"
-            >
-              Log In
-            </Button>
-            <Button
-              color="inherit"
-              sx={{
-                backgroundColor: colors.highlight,
-                color: colors.primary,
-                textTransform: "none",
-                fontSize: "12px",
-                borderRadius: "5px",
-                px: 1,
-                py: 1,
-                "&:hover": {
-                  backgroundColor: "#FFD700",
-                  boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
-                },
-              }}
-              href="/sign-up"
-            >
-              Sign Up
-            </Button>
+            <SignInButton>
+              <Button
+                color="inherit"
+                sx={{
+                  textTransform: "none",
+                  fontSize: "12px",
+                }}
+              >
+                Log In
+              </Button>
+            </SignInButton>
+            <SignUpButton>
+              <Button
+                color="inherit"
+                sx={{
+                  textTransform: "none",
+                  fontSize: "12px",
+                  borderRadius: "5px",
+                  px: 1,
+                  py: 1,
+                  "&:hover": {
+                    backgroundColor: "#FFD700",
+                    boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
+                  },
+                }}
+              >
+                Sign Up
+              </Button>
+            </SignUpButton>
           </SignedOut>
           <SignedIn>
             <UserButton userProfileUrl="/user-profile" />
--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import TopNav from "./components/TopNav";
 
 export default function Home() {
