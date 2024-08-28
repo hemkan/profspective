11f0e90db0b8a764396fcadc1b342fa1bff24691
GauravKano <gaurav.kanoujia2006@gmail.com>
2024-08-28 00:03:47 -0400

    Add chatbot to the classes page

--- app/classes/page.js
+++ app/classes/page.js
@@ -1,9 +1,31 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import TopNav from "../components/TopNav";
 import ClassList from "../components/ClassList";
-import { Box } from "@mui/material";
+import { Box, Fab, Slide } from "@mui/material";
+import ChatBot from "../components/ChatBot";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 
 export default function Classes() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [isFabOpen, setIsFabOpen] = useState(true);
+
+  const closeChat = () => {
+    setIsChatOpen(false);
+
+    setTimeout(() => {
+      setIsFabOpen(true);
+    }, 280);
+  };
+
+  const openChat = () => {
+    setIsFabOpen(false);
+
+    setTimeout(() => {
+      setIsChatOpen(true);
+    }, 200);
+  };
+
   return (
     <Box
       paddingBottom={3}
@@ -17,6 +39,33 @@ export default function Classes() {
     >
       <TopNav />
       <ClassList />
+      <Slide in={isChatOpen} direction="up" mountOnEnter unmountOnExit>
+        <Box
+          sx={{
+            position: "fixed",
+            bottom: { xs: "0px", sm: "15px" },
+            right: { xs: "0px", sm: "25px" },
+            zIndex: 1000,
+            borderRadius: { xs: "0px", sm: "20px" },
+            boxShadow: "0 0 3px white",
+          }}
+        >
+          <ChatBot closeChat={closeChat} />
+        </Box>
+      </Slide>
+      <Slide in={isFabOpen} direction="up" mountOnEnter unmountOnExit>
+        <Fab
+          sx={{
+            position: "fixed",
+            bottom: "25px",
+            right: "25px",
+            boxShadow: "0 0 3px white",
+          }}
+          onClick={openChat}
+        >
+          <ChatBubbleIcon />
+        </Fab>
+      </Slide>
     </Box>
   );
 }
